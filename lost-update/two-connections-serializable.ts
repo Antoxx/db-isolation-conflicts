957c3032c776fd6b4607638da5902e371d403efe
Anton Stydenko <stydenko.anton@gmail.com>
2025-05-05 15:55:20 +0300

    Add errors

--- lost-update/two-connections-serializable.ts
+++ lost-update/two-connections-serializable.ts
@@ -12,17 +12,23 @@ await Promise.all([
   increaseBalance(client2, 10.1, 0, ISOLATION_LEVEL.SERIALIZABLE),
 ])
 
-balance = await logBalance(client1) // 101.01 - the first is a winner with SERIALIZABLE
+balance = await logBalance(client1) 
+// 101.01
+// the first is a winner with SERIALIZABLE
+// the second failed with error "could not serialize access due to concurrent update"
 assert.equal(balance, 101.01)
 
 await resetBalance(client1)
 
 await Promise.all([
-  increaseBalance(client1, 1.01, 100, ISOLATION_LEVEL.REPEATABLE_READ, 30),
-  increaseBalance(client2, 10.1, 500, ISOLATION_LEVEL.REPEATABLE_READ, 1),
+  increaseBalance(client1, 1.01, 100, ISOLATION_LEVEL.SERIALIZABLE, 30),
+  increaseBalance(client2, 10.1, 500, ISOLATION_LEVEL.SERIALIZABLE, 1),
 ])
 
-balance = await logBalance(client1) // 110.1 - the second is a winner with SERIALIZABLE (started later but finished fast), the first failed with error
+// 110.1
+// the second is a winner with SERIALIZABLE (started later but finished fast)
+// the first failed with error "could not serialize access due to concurrent update"
+balance = await logBalance(client1) 
 assert.equal(balance, 110.1)
 
 await client1.end()
